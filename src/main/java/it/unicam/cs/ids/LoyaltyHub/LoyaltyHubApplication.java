78490b62d297a5db8c097d746dbdd4cad4dcf9f4
PaoloSilenzi <94998347+PaoloSilenzi@users.noreply.github.com>
2023-11-24 00:09:19 +0100

    First commit

---
+++ src/main/java/it/unicam/cs/ids/LoyaltyHub/LoyaltyHubApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.LoyaltyHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyHubApplication.class, args);
+
+	}
+}
---
+++ src/test/java/it/unicam/cs/ids/LoyaltyHub/LoyaltyHubApplicationTests.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.ids.LoyaltyHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyHubApplicationTests {
+
+	//TODO
+
+}

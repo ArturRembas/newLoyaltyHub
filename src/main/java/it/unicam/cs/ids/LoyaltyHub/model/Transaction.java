2589491a636004c190201c0b565bb88819a576ab
ArturRembas <94998370+ArturRembas@users.noreply.github.com>
2024-02-28 00:17:37 +0100

    fix

--- src/main/java/it/unicam/cs/ids/LoyaltyHub/model/Transaction.java
+++ src/main/java/it/unicam/cs/ids/LoyaltyHub/model/Transaction.java
@@ -1,42 +1,56 @@
 package it.unicam.cs.ids.LoyaltyHub.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
- * Serves as the base class for users within the loyalty platform system.
- * This abstract class provides common user properties and functionalities.
+ * Represents a transaction within the loyalty platform, linking activities,
+ * fidelity cards, and their corresponding prices and validation statuses.
  */
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-public abstract class User {
+public class Transaction {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "progressiveNumber")
-    @SequenceGenerator(name = "progressiveNumber", allocationSize = 1)
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "transactionGenerator")
+    @SequenceGenerator(name = "transactionGenerator", allocationSize = 1)
+    @Column(name = "transaction_id", nullable = false)
+    private Long transactionId;
 
-    private String name;
-    private String surname;
-    private String email;
-    private String phone;
+    private int price;
+    private boolean validated = false;
+
+    @ManyToOne
+    @JoinColumn(name = "fidelity_card_card_id")
+    @JsonIgnore
+    private FidelityCard fidelityCard;
+
+    @OneToOne(orphanRemoval = true)
+    @JsonIgnore
+    @JoinColumn(name = "activity_user_id")
+    private Activity activity;
 
     /**
-     * Constructs a new User with the specified details.
+     * Constructs a new Transaction with the specified price, fidelity card, and activity.
      *
-     * @param name    the first name of the user
-     * @param surname the surname of the user
-     * @param email   the email address of the user
-     * @param phone   the phone number of the user
+     * @param price         the price associated with the transaction
+     * @param fidelityCard  the fidelity card involved in the transaction
+     * @param activity      the activity linked to the transaction
      */
-    public User(String name, String surname, String email, String phone) {
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.phone = phone;
+    public Transaction(int price, FidelityCard fidelityCard, Activity activity) {
+        this.price = price;
+        this.fidelityCard = fidelityCard;
+        this.activity = activity;
+    }
+
+    /**
+     * Marks this transaction as validated.
+     */
+    public void validate() {
+        this.validated = true;
     }
 }
--- src/main/java/it/unicam/cs/ids/LoyaltyHub/model/User.java
+++ src/main/java/it/unicam/cs/ids/LoyaltyHub/model/User.java
@@ -1,10 +1,42 @@
 package it.unicam.cs.ids.LoyaltyHub.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 /**
- * Represents a generic user in the LoyaltyHub system.
- * This interface serves as a base for different types of users within the system,
- * allowing for future expansion and the addition of specific user roles.
+ * Serves as the base class for users within the loyalty platform system.
+ * This abstract class provides common user properties and functionalities.
  */
-public interface User {
-    
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "progressiveNumber")
+    @SequenceGenerator(name = "progressiveNumber", allocationSize = 1)
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    private String name;
+    private String surname;
+    private String email;
+    private String phone;
+
+    /**
+     * Constructs a new User with the specified details.
+     *
+     * @param name    the first name of the user
+     * @param surname the surname of the user
+     * @param email   the email address of the user
+     * @param phone   the phone number of the user
+     */
+    public User(String name, String surname, String email, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+    }
 }
